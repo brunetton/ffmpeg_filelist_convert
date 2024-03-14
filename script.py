44cb370ecec2b9e29c93fa02dca189578ba0659c
Bruno <bruno@brunetton.org>
2024-03-14 23:42:50 +0100

    Enhance duration output

--- script.py
+++ script.py
@@ -23,9 +23,9 @@ with file_list.open() as f:
         out_file = Path(out_dir / out_file.parent / Path(out_file.stem + '.mp4'))   # out/DD2/FAE_20231026_002/NINJAV_S001_S001_T002.mp4
         tmp_file = f"tmp/{out_file.name}"                                           # tmp/NINJAV_S001_S001_T002.mp4
         os.system(f"rm -rf tmp/*")
-        print("Duration: ", end="")
-        os.system(f"ffprobe -i {file} -show_entries format=duration -v quiet -of csv=\"p=0\"")
-        command = f"ffpb -i {line} -c:v libx264 -pix_fmt yuv420p -crf 20 -map v:0 -map a:0 -c:a aac -map_channel 0.0.2 -b:a 128k -vf \"scale=960:540\"  {tmp_file}"
+        duration = subprocess.check_output(f"ffprobe -i {file} -show_entries format=duration -v quiet -of csv=\"p=0\"", shell=True)
+        print("Duration: {0:.1f}m".format(float(duration.decode('utf8')) / 60))
+        command = f"ffpb -i {line} {ffmpeg_options} {tmp_file}"
         print(command)
         errors = subprocess.call(command, shell=True)
         assert not errors
